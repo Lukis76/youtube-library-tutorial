43efccf14ef03b3b57bbb821693729169b9c60f8
Lucas Ruben Manoguerra <kiu.manoguerra@gmail.com>
2023-10-19 18:16:18 -0300

    feat: add testing by vitest and storybook workspace and github actions main CI

---
+++ vitest.workspace.ts
@@ -0,0 +1,20 @@
+import { defineWorkspace } from 'vitest/config'
+
+// defineWorkspace provides a nice type hinting DX
+export default defineWorkspace([
+  'packages/**/*',
+  {
+    // add "extends" to merge two configs together
+    extends: './vitest.config.ts',
+    test: {
+      include: [
+        'packages/**/**/__tests__/*.{test,spec}.{js,ts,jsx,tsx,mjs,cjs,mts,cts}',
+        'apps/**/**/__tests__/*.{test,spec}.{js,ts,jsx,tsx,mjs,cjs,mts,cts}',
+        '__tests__/*.{test,spec}.{js,ts,jsx,tsx,mjs,cjs,mts,cts}'
+      ],
+      // it is recommended to define a name when using inline configs
+      name: 'happy-dom',
+      environment: 'happy-dom'
+    }
+  }
+])
